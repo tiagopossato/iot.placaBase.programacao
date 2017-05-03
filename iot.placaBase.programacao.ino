4176207ee197cf572bd14b3144dd03c26c197cc7
nectar <nectar@Nectar-PC>
2017-05-02 21:32:35 -0300

    novo protocolo

--- iot.placaBase.programacao.ino
+++ iot.placaBase.programacao.ino
@@ -8,7 +8,7 @@
 //#define DEBUG
 
 #define CENTRAL_ID 0x00
-#define INTERVALO_ONLINE 500 //Intervalo para enviar sinal de online
+#define INTERVALO_ONLINE 2000 //Intervalo para enviar sinal de online
 
 String inputString = "";// a string to hold incoming data
 
@@ -60,11 +60,15 @@ void setup()
 }
 
 void isOnline() {
-  Serial.print(F("{\"id\":"));
+  Serial.print(F("["));
   Serial.print(CENTRAL_ID);
-  Serial.print(F(", \"codigo\":"));
-  Serial.print(ONLINE);
-  Serial.print(F("}\n"));
+  Serial.print(F("/"));
+  Serial.print(F("9999"));
+  Serial.print(F("/"));
+  Serial.print(F("1"));
+  Serial.print(F("/"));
+  Serial.print(F("1"));
+  Serial.println(F("]"));
   Serial.flush();
 }
 
@@ -78,28 +82,33 @@ void loop()
   if (CAN.checkReceive() == CAN_MSGAVAIL) {
 
     CAN.readMsgBuf(&len, rawMsg);    // read data,  len: data length, buf: data buf
-
-    canMsg.id = CAN.getCanId();
-    canMsg.codigo = rawMsg[0];
-    for (char i = 1; i < len; i++) {
-      canMsg.msg[i - 1] = rawMsg[i];
+    Serial.print("[");
+    Serial.print(CAN.getCanId());
+    Serial.print("/");
+    if (rawMsg[0] == 99) {
+      Serial.print("9999");
+    }
+    else {
+      Serial.print(rawMsg[0] + 3200);
     }
 
-    Serial.print(F("{\"id\":"));
-    Serial.print(canMsg.id);
-    Serial.print(F(", \"codigo\":"));
-    Serial.print(canMsg.codigo);
-    Serial.print(F(", \"msg\":["));
-    for (char i = 0; i < len - 1; i++) {
-      Serial.print(canMsg.msg[i]);
-      if (i < len - 2)Serial.print(F(", "));
+    Serial.print("/");
+    //se for valor analógico
+    if (rawMsg[0] == 2) {
+      Serial.print(rawMsg[1] + 3300);
+      Serial.print("/");
+      Serial.print(word(rawMsg[2], rawMsg[3]) / 100.0);
+    } else {
+      Serial.print(rawMsg[1]);
+      Serial.print("/");
+      Serial.print(rawMsg[2]);
     }
-    Serial.print(F("]}\n"));
+    Serial.println("]");
     Serial.flush();
   }
-  
+
   //envia sinal de online a cada tempo predeterminado
-  if (millis() - previousMillis >= INTERVALO_ONLINE) {    
+  if (millis() - previousMillis >= INTERVALO_ONLINE) {
     previousMillis = millis();
     isOnline();
   }
